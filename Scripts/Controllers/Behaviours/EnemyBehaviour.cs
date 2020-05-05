d68e3ebf860a10c98008f0ffb8d43cf9d33cc00f
DeRestStorm <deidara0909@hotmail.com>
2020-05-05 19:36:58 +0300

    фикс преследователя

--- Scripts/Controllers/Behaviours/EnemyBehaviour.cs
+++ Scripts/Controllers/Behaviours/EnemyBehaviour.cs
@@ -1,9 +1,16 @@
+using BezierSolution;
 using UnityEngine;
 
 namespace Controllers.Behaviours
 {
     public class EnemyBehaviour : MonoBehaviour
     {
-        
+        private void Start()
+        {
+            var walker= GetComponent<BezierWalkerWithSpeed>();
+            walker.NormalizedT = 0;
+            walker.enabled = true;
+            
+        }
     }
 }
