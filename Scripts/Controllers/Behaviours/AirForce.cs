67c3f41e61e325ca294badfee3338dc70345bec4
DeRestStorm <deidara0909@hotmail.com>
2020-05-29 15:58:34 +0300

    air

---
+++ Scripts/Controllers/Behaviours/AirForce.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+namespace Controllers.Behaviours
+{
+    public class Air : MonoBehaviour
+    {
+        public float Force;
+
+        private void OnTriggerStay(Collider other)
+        {
+            var rb = other.GetComponent<Rigidbody>();
+            if (rb == null)
+                return;
+            rb.AddForce(transform.forward * Force);
+        }
+        
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.matrix = transform.localToWorldMatrix;
+
+            var collider = GetComponent<BoxCollider>();
+            if (collider != null)
+            {
+                Gizmos.DrawWireCube(collider.center, collider.size);
+            }
+
+            DrawArrow.ForGizmo(Vector3.zero, Vector3.forward, Color.cyan, .5f);
+        }
+    }
+}
